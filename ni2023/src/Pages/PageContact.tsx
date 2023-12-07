a6aca642eeb91ab5e2a354172ed232f1685bf6d6
chihiroprog <stutuart.barthe@gmail.com>
2023-12-07 23:24:19 +0100

    achitecture

---
+++ ni2023/src/Pages/PageContact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PageContact({ redirection }) {
+  return (
+    <div className="PageContact">
+      {/* Your content here */}
+      azeazea
+    </div>
+  );
+}
+
+export default PageContact;
