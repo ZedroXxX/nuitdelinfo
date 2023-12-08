74e587b2c10eaebfcd471e6e5d6c5bdeeee6cb79
Soltan Hamadouche <soltan.hamadouche@gmail.com>
2023-12-08 05:37:00 +0100

    tqt

--- ni2023/src/Pages/PageMentionLegales.tsx
+++ ni2023/src/Pages/PageMentionLegales.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-function PageMesures ({redirection}:any) {
+function PageMentionsLegales() {
+  return (
+    <div className="PageMentionsLegales">
+      <h1>Mentions Légales</h1>
+      <p>Ceci est la page de mentions légales de notre site web.</p>
+      <p>Vous pouvez ajouter ici toutes les informations légales requises, telles que les coordonnées de l'entreprise, les mentions de copyright, la politique de confidentialité, etc.</p>
+      <p>Assurez-vous de personnaliser cette page en fonction des exigences légales de votre site.</p>
+    </div>
+  );
+}
 
-    return (
-      <div className="PageMesures">
-        {/* Votre contenu ici */}
-        eazeazeae
-      </div>
-    );
-  }
-
-
-export default PageMesures;
+export default PageMentionsLegales;
